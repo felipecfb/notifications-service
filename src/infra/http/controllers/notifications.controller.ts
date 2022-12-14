a031e2b0c63357f04eb671e65047701b3f2cf294
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-12-14 11:56:01 -0300

    ✨ feat: importando um módulo no outro e removendo erros da aplicação

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,10 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
 import { createNotificationBody } from '../dtos/create-notification-body';
 
 @Controller('notifications')
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @Post()
   async create(@Body() body: createNotificationBody) {
     const { recipientId, content, category } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return { notification };
   }
 }
